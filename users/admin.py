69fc961635771593b0bdedcf2e03112ed0b4099d
5no0p <mmoaaa1989@hotmail.com>
2022-11-08 11:07:31 +0200

    graphene

--- users/admin.py
+++ users/admin.py
@@ -4,8 +4,3 @@ from django.apps import apps
 from users.models import User
 
 admin.site.register(User)
-
-app = apps.get_app_config('graphql_auth')
-
-for model_name, model in app.models.items():
-    admin.site.register(model)
